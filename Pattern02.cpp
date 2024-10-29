6eecc063c503432f20bf62125e297997147cd63e
Md Tareq Miah <mdtareqmia25@gmail.com>
2024-10-29 22:26:28 +0600

    4Pattern_Printing

--- Pattern02.cpp
+++ Pattern02.cpp
@@ -3,17 +3,18 @@ using namespace std;
 int main(){
     int n;
     cin>>n;
-    for(int i=1; i<=n; i++){
-        for(int j=0; j<i; j++){
+    for(int i=0; i<n; i++){
+        for(int j=n; j>i; j--){
             cout<<"* ";
         }
         cout<<endl;
     }
+    return 0;
 }
 
-
-// *
-// * *
-// * * *
-// * * * *
+// * * * * * *
 // * * * * *
+// * * * *
+// * * * 
+// * *
+// *
